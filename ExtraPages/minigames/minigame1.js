b3e5c807270f2c75dfa2e78c04d3d45c402b286a
Guyingreen <147945223+guyingreen132@users.noreply.github.com>
2023-12-03 11:43:19 -0700

    Update minigame1.js

--- ExtraPages/minigames/minigame1.js
+++ ExtraPages/minigames/minigame1.js
@@ -18,5 +18,5 @@ function draw() {
 background(back);
   image(img, mouseX-50, mouseY-50,100,100);
     image(img, x,y,100,100);
-  if (mouseX>x&&,mouseY<x+100) {x=random(100, 900);}
+  if (mouseX>x&&,mouseY<x+100) {let x = random(width);}
 }
