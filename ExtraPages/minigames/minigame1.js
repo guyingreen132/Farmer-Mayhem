4afa1afa112451789f7845fc06bc189b9060f59a
Guyingreen <147945223+guyingreen132@users.noreply.github.com>
2023-12-03 11:48:39 -0700

    Update minigame1.js

--- ExtraPages/minigames/minigame1.js
+++ ExtraPages/minigames/minigame1.js
@@ -1,22 +1,80 @@
-let img;
-let back;
-let x=0;
-let y=0;
+const canvas = document.getElementById('gameCanvas');
+const ctx = canvas.getContext('2d');
 
-function preload() {
-  img = loadImage('../../assets/Player.png');
-  back = loadImage('../../assets/Grass.png');
-  plants = loadImage('../../assets/plant.png');
+// Set canvas dimensions
+canvas.width = window.innerWidth;
+canvas.height = window.innerHeight;
+
+// Load images
+const backgroundImage = new Image();
+backgroundImage.src = "../../assets/Grass.png";
+
+const playerImage = new Image();
+playerImage.src = "../../assets/Sell.png";
+
+// Player object
+const player = {
+    x: 50,
+    y: 50,
+    width: 50,
+    height: 50,
+    speed: 5
+};
+
+// Update function
+function update() {
+    // Clear the canvas
+    ctx.clearRect(0, 0, canvas.width, canvas.height);
+
+    // Draw background
+    ctx.drawImage(backgroundImage, 0, 0, canvas.width, canvas.height);
+
+    // Draw player
+    ctx.drawImage(playerImage, player.x, player.y, player.width, player.height);
+
+    // Move player towards the mouse
+    const dx = mouseX - player.x - player.width / 2;
+    const dy = mouseY - player.y - player.height / 2;
+    const distance = Math.sqrt(dx * dx + dy * dy);
+    if (distance > player.speed) {
+        player.x += (dx / distance) * player.speed;
+        player.y += (dy / distance) * player.speed;
+    }
+
+    // Check for collision with the player
+    if (
+        player.x < mouseX &&
+        player.x + player.width > mouseX &&
+        player.y < mouseY &&
+        player.y + player.height > mouseY
+    ) {
+        // Game over
+        alert("Game Over! The player caught you.");
+        resetGame();
+    }
+
+    // Request next animation frame
+    requestAnimationFrame(update);
 }
 
-
-function setup () {
-createCanvas(1000,400);
+// Reset the game
+function resetGame() {
+    player.x = 50;
+    player.y = 50;
 }
 
-function draw() {
-background(back);
-  image(img, mouseX-50, mouseY-50,100,100);
-    image(img, x,y,100,100);
-  if (mouseX>x&&,mouseY<x+100) {let x = random(width);}
+// Mouse position
+let mouseX = 0;
+let mouseY = 0;
+
+// Update mouse position
+function updateMousePosition(event) {
+    mouseX = event.clientX;
+    mouseY = event.clientY;
 }
+
+// Event listeners
+window.addEventListener('mousemove', updateMousePosition);
+
+// Start the game loop
+update();
