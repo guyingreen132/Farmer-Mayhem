14fc50afe440b4f67a2685e3056ccd92d88a8822
Guyingreen <147945223+guyingreen132@users.noreply.github.com>
2023-12-03 11:39:29 -0700

    Update minigame1.js

--- ExtraPages/minigames/minigame1.js
+++ ExtraPages/minigames/minigame1.js
@@ -1,9 +1,12 @@
 let img;
 let back;
+let x=0;
+let y=0;
 
 function preload() {
   img = loadImage('../../assets/Player.png');
   back = loadImage('../../assets/Grass.png');
+  plants = loadImage('../../assets/plant.png');
 }
 
 
@@ -14,4 +17,6 @@ createCanvas(1000,400);
 function draw() {
 background(back);
   image(img, mouseX-50, mouseY-50,100,100);
+    image(img, x,y,100,100);
+  if (mouseX>x&&,mouseY<x+100) {x=random(100, 900);}
 }
